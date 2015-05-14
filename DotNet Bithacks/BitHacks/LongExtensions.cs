7ad74ce81d02025fc5b268814798bb5f0546559f
Brandon Hurlburt <brandon_hurlburt@starkey.com>
2015-05-14 13:59:25 -0500

    Initial checkin - IsPowerOf2 working for Unsigned Longs

---
+++ DotNet Bithacks/BitHacks/LongExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace BitHacks
+{
+    public static class LongExtensions
+    {
+        /// <summary>Number of bits per byte</summary>
+        const byte CHAR_BIT = 8;
+
+        public static bool IsPowerOf2(this ulong v)
+        {
+            return v > 0 && ((v & (v - 1)) == 0);
+        }
+    }
+}
---
+++ DotNet Bithacks/UnitTests/LongTests.cs
@@ -0,0 +1,17 @@
+﻿using BitHacks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class LongTests
+    {
+        [TestMethod]
+        public void IsPowerOf()
+        {
+            Assert.IsTrue(4UL.IsPowerOf2());
+            Assert.IsFalse(3UL.IsPowerOf2());
+            Assert.IsFalse(0UL.IsPowerOf2());
+        }
+    }
+}
